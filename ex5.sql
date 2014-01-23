36c8535a903229928adcccadf38466957840c6f7
Prescott Murphy <prurph@gmail.com>
2014-01-23 18:18:02 -0500

    Finish exercise 5; parentheses can be used in statements with multiple conditionals and function as expected

---
+++ ex5.sql
@@ -0,0 +1,10 @@
+SELECT * FROM person;
+SELECT name, age FROM pet;
+SELECT name, age FROM pet WHERE dead = 0;
+SELECT * FROM person WHERE first_name != "Zed";
+SELECT * FROM pet WHERE age > 10;
+SELECT * FROM person WHERE age < 31;
+SELECT * FROM person WHERE age > 31;
+SELECT * FROM person WHERE age = 31 AND first_name = 'Prescott';
+SELECT * FROM pet WHERE age > 6 OR (dead = 1 AND name = 'Henry');
+SELECT * FROM pet WHERE (age > 6 OR dead = 1) AND name = 'Henry';
