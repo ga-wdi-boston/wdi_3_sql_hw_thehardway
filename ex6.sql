3a02ac1b6dc90e28f0e088abdfa25b7141348031
Mike Stone <mikestone14@gmail.com>
2014-01-23 18:52:29 -0500

    Exercise 7 complete

--- ex6.sql
+++ ex6.sql
@@ -1,8 +1,8 @@
-INSERT INTO person_pet (person_id, pet_id) VALUES (3, 2);
+INSERT INTO person_pet (person_id, pet_id) VALUES (1, 2);
 
 SELECT pet.id, pet.name, pet.age, pet.dead
     FROM pet, person_pet, person
     WHERE
     pet.id = person_pet.pet_id AND
     person_pet.person_id = person.id AND
-    person.id = 3;
+    person.id = 1;
