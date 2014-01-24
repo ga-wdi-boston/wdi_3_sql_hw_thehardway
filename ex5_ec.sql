96f3fa41ec62437682679ed3d338ee998451caaa
Janice Smith <janice.k.n.smith@gmail.com>
2014-01-23 19:21:33 -0500

    ex.5 done with ec

---
+++ ex5_ec.sql
@@ -0,0 +1,6 @@
+
+
+SELECT * FROM person WHERE age > 27;
+SELECT * FROM person WHERE age < 27;
+SELECT name, age FROM pet WHERE age > 10;
+SELECT name, age FROM pet WHERE age > 0 AND id > 0 OR id = 2;
