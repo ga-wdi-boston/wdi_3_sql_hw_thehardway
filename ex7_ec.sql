11b7f3cc80fe3625189d89ec0dbfebe1e6095d55
Janice Smith <janice.k.n.smith@gmail.com>
2014-01-23 19:41:26 -0500

    ex.7 with ec

---
+++ ex7_ec.sql
@@ -0,0 +1,62 @@
+/* Combine all of ex2.sql through ex7.sql into */
+/*one file and redo the above script so you just run this one new file to recreate the database*/
+
+DROP TABLE person;
+DROP TABLE pet;
+DROP TABLE person_pet;
+
+
+CREATE TABLE person (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE pet (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT,
+    age INTEGER,
+    dead INTEGER
+);
+
+CREATE TABLE person_pet (
+    person_id INTEGER,
+    pet_id INTEGER
+);
+
+INSERT INTO person (id, first_name, last_name, age)
+    VALUES (0, "Zed", "Shaw", 37);
+
+INSERT INTO pet (id, name, breed, age, dead)
+    VALUES (0, "Fluffy", "Unicorn", 1000, 0);
+
+INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
+
+DELETE FROM pet WHERE id = 0;
+DELETE FROM pet WHERE id = 1;
+
+INSERT INTO pet (id, name, breed, age, dead)
+    VALUES (0, "Fluffy", "Unicorn", 1000, 0);
+
+INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
+
+INSERT INTO person_pet (person_id, pet_id) VALUES (0, 0);
+INSERT INTO person_pet VALUES (0, 1);
+
+SELECT * FROM person;
+
+SELECT name, age FROM pet;
+
+SELECT name, age FROM pet WHERE dead = 0;
+
+SELECT * FROM person WHERE first_name != "Zed";
+
+SELECT pet.id, pet.name, pet.age, pet.dead
+    FROM pet, person_pet, person
+    WHERE
+    pet.id = person_pet.pet_id AND
+    person_pet.person_id = person.id AND
+    person.first_name = "Zed";
+
