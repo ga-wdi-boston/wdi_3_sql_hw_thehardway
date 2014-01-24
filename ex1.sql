24d093fb3a4192a7a38331de723ff74c09668591
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-01-24 09:58:34 -0500

    first extra credit redone

--- ex1.sql
+++ ex1.sql
@@ -1,1 +1,1 @@
-CREATE TABLE person (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, age INTEGER);
+create table person (id integer primary key, first_name text, last_name text, age integer);
