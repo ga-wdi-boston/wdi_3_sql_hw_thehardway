6ea6930a1faee917d59bc177c62eed69a2fe4106
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-23 19:54:58 -0500

    updated ex3 and ex4

---
+++ ex3.sql
@@ -0,0 +1,24 @@
+INSERT INTO person (id, first_name, last_name, age)
+    VALUES (0, "Zed", "Shaw", 37);
+
+INSERT INTO pet (id, name, breed, age, dead)
+    VALUES (0, "Fluffy", "Unicorn", 1000, 0);
+
+INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
+
+Extra Credit
+
+INSERT INTO person (id, first_name, last_name, age)
+    VALUES (1, "Praggya", "Barretto", 34);
+
+INSERT INTO person (id, first_name, last_name, age)
+    VALUES (2, "Jennifer", "Steinberg", 35);
+
+INSERT INTO pet (id, name, breed, age, dead)
+    VALUES (2, "Stella", "Cat", 1, 0);
+
+INSERT INTO pet (id, name, breed, age, dead)
+    VALUES (3, "Hamlet", "Hamster", 15, 1);
+
+INSERT INTO pet (id, name, breed, age, dead)
+    VALUES (4, "Omelet", "Hamster", 15, 1);
---
+++ ex4.sql
@@ -0,0 +1,8 @@
+INSERT INTO person_pet (person_id, pet_id) VALUES (0, 0);
+INSERT INTO person_pet VALUES (0, 1);
+
+Extra Credit
+
+INSERT INTO person_pet (person_id, pet_id) VALUES (1, 2);
+INSERT INTO person_pet (person_id, pet_id) VALUES (2, 3);
+INSERT INTO person_pet (person_id, pet_id) VALUES (2, 4);
