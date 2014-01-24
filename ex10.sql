ef6f4ec690cd17709a3a78a7fca670b8f43f6604
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-23 23:31:52 -0500

    ex10

---
+++ ex10.sql
@@ -0,0 +1,8 @@
+sqlite> select * from pet;
+6|Zed's Pet|Unicorn|1000|0
+7|Zed's Pet|Robot|1|0
+10|Stella|Cat|1|0
+11|Deceased|Hamster|15|1
+12|Deceased|Hamster|15|1
+
+I wasn't sure how to do the extra credit on this one since there are no dead pets owned by Zed.
