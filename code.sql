6c81a51870f05a29770facb232db9409de08084b
Becky <email@email.com>
2014-01-23 21:28:35 -0500

    updated code.sql with alter table data, created ex12.sql

--- code.sql
+++ code.sql
@@ -1,9 +1,18 @@
+DROP TABLE IF EXISTS person;
+
 CREATE TABLE person (
     id INTEGER PRIMARY KEY,
     first_name TEXT,
     last_name TEXT,
     age INTEGER
 );
+
+ALTER TABLE person ADD COLUMN height INTEGER;
+ALTER TABLE person ADD COLUMN weight INTEGER;
+
+
+
+DROP TABLE IF EXISTS person;
 
 CREATE TABLE pet (
     id INTEGER PRIMARY KEY,
@@ -13,38 +22,10 @@ CREATE TABLE pet (
     dead INTEGER
 );
 
+DROP TABLE IF EXISTS person;
+
 CREATE TABLE person_pet (
     person_id INTEGER,
     pet_id INTEGER
 
-INSERT INTO person (id, first_name, last_name, age)
-    VALUES (0, "Zed", "Shaw", 37);
 
-INSERT INTO pet (id, name, breed, age, dead)
-    VALUES (0, "Fluffy", "Unicorn", 1000, 0);
-
-INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
-
-INSERT INTO person (id, first_name, last_name, age)
-    VALUES (1, "Janet", "Rollins", 99);
-
-INSERT INTO pet (id, name, breed, age, dead)
-    VALUES (1, "Rex", "Alligator", 10, 1);
-
-INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
-
-
-UPDATE person SET first_name = "Hilarious Guy"
-    WHERE first_name = "Zed";
-
-UPDATE pet SET name = "Fancy Pants"
-    WHERE id=0;
-
-SELECT * FROM person;
-SELECT * FROM pet;
-
-UPDATE person SET first_name = "Zed"
-	WHERE id = 0
-
-UPDATE pet SET name = "Rhonda"
-	WHERE id = 0;
