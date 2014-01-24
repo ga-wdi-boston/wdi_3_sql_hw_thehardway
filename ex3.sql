a84592719acb74adbc37c5da78bcf494d9d9aaae
Jordan Morano <jordan.morano@gmail.com>
2014-01-23 21:54:23 -0500

    bonus for ex3

--- ex3.sql
+++ ex3.sql
@@ -5,3 +5,10 @@ INSERT INTO pet (id, name, breed, age, dead)
   VALUES (0, 'Fluffy', 'Unicorn', 1000, 0);
 
 INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 1);
+
+INSERT INTO person VALUES (1, 'Jordan', 'Morano', 31);
+
+INSERT INTO pet VALUES (2, 'Rosie', 'American Bulldog', 12, 0);
+
+INSERT INTO person_pet (person_id, pet_id)
+  VALUES (1, 2);
